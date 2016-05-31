b15ccbba42e578106fd9f78dbdcf6cb7ce62d4e4
Sebastian Rühmann <mail@sebastianruehmann.de>
2016-05-31 19:58:14 +0200

    change  server sendMail file from GET to POST protocol

--- src/send.php
+++ src/send.php
@@ -1,7 +1,7 @@
 <?php
-$name = $_GET["name"];
-$email = $_GET["email"];
-$message = $_GET["message"];
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
 
 if(strlen($name) >= 4 && strlen($message) >= 15 && strlen($email) && preg_match("/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i",$email)) {
     mail("mail@sebastianruehmann.de","Kontaktanfrage von ". $name, $message, 'From: '. $email . "\n" . 'Content-Type:text/plain; charset="UTF-8"');
